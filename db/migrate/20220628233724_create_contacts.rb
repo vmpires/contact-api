f02aec062e7a38f143a0b630afe8eb72ba428054
Victor Pires <victor.pires@grana.capital>
2022-06-28 21:20:31 -0300

    first commit

---
+++ db/migrate/20220628233724_create_contacts.rb
@@ -0,0 +1,12 @@
+class CreateContacts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :contacts do |t|
+      t.string :name
+      t.string :phone
+      t.text :description
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20220629001157_add_emailto_contacts.rb
@@ -0,0 +1,4 @@
+class AddEmailtoContacts < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
